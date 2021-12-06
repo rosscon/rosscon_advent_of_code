72cbea3d7b86a12fdb01d44a68c688a4503b0e3d
Ross Conroy <9072796+rosscon@users.noreply.github.com>
2021-12-06 16:21:20 +0000

    2021 day 5 complete

---
+++ 2021/day_05/day_05_part_02.py
@@ -0,0 +1,73 @@
+#
+# Parse the puzzle input
+#
+import sys
+
+fileName = "input.txt"
+
+infile = open(fileName, 'r')
+input_data = [line.strip() for line in infile.readlines()]
+
+vent_lines = [[[int(z) for z in y.split(',')] for y in x.split(' -> ')] for x in input_data]
+
+
+def is_horizontal(c1, c2):
+    return c1[1] == c2[1]
+
+
+def is_vertical(c1, c2):
+    return c1[0] == c2[0]
+
+
+def is_diagonal(c1, c2):
+    return abs(c1[0] - c2[0]) == abs(c1[1] - c2[1])
+
+
+# Process each vent line
+
+grid = {}
+
+for vent_line in vent_lines:
+
+    line_points = []
+
+    if is_horizontal(vent_line[0], vent_line[1]):
+
+        y = vent_line[0][1]
+        x1 = min(vent_line[0][0], vent_line[1][0])
+        x2 = max(vent_line[0][0], vent_line[1][0])
+
+        for x in range(x1, x2 + 1):
+            line_points.append((x, y))
+
+    elif is_vertical(vent_line[0], vent_line[1]):
+
+        x = vent_line[0][0]
+        y1 = min(vent_line[0][1], vent_line[1][1])
+        y2 = max(vent_line[0][1], vent_line[1][1])
+
+        for y in range(y1, y2 + 1):
+            line_points.append((x, y))
+
+    elif is_diagonal(vent_line[0], vent_line[1]):
+
+        c1 = vent_line[0] if vent_line[0][0] < vent_line[1][0] else vent_line[1]
+        c2 = vent_line[0] if vent_line[0][0] > vent_line[1][0] else vent_line[1]
+
+        gradient = 1 if c1[1] < c2[1] else -1
+        dx = c2[0] - c1[0]
+
+        for x in range(dx + 1):
+            line_points.append((c1[0] + x, c1[1] + (x * gradient)))
+
+    else:
+        print("ERROR")
+
+    for line_point in line_points:
+        if line_point not in grid:
+            grid[line_point] = 1
+        else:
+            grid[line_point] = grid[line_point] + 1
+
+overlaps = [x_key for x_key, x_val in grid.items() if x_val > 1]
+print(len(overlaps))
